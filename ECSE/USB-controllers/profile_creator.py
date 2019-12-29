d1a5ac220ac083506b8ca63f5476db9a903ee70c
Matthias Arabian <matthias15002@gmail.com>
2019-12-28 23:49:33 -0500

    Update profile_creator

--- ECSE/USB-controllers/profile_creator.py
+++ ECSE/USB-controllers/profile_creator.py
@@ -1,1 +1,10 @@
-import math
+from inputs import devices
+from inputs import get_gamepad
+
+for device in devices:
+    print(device)
+
+#while 1:
+    #events = get_gamepad()
+    #for event in events:
+    #    print(event.ev_type, event.code, event.state)
