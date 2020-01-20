34a7b9a82459f1213ef19b48bba4229140974abb
Matthias Arabian <matthias15002@gmail.com>
2020-01-20 15:04:21 -0500

    Add to workshops. Add example code

---
+++ ECSE/Workshop-2/input-output-example/input-output-example.ino
@@ -0,0 +1,20 @@
+#define pin 2
+#define ledPin 13
+int ledState;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+  ledState = LOW;
+  digitalWrite(ledPin, ledState);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (digitalRead(pin) != ledState){
+    ledState = digitalRead(pin);
+    digitalWrite(ledPin, !ledState);
+    delay(50);
+  }
+}
